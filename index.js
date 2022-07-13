0e953dfdf621244c65e1cc5668ce024cfc193296
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-12 22:52:31 -0700

    css

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 //const Datastore = require('nedb');
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/css/index.css', function (req, res) {
-    res.sendFile(__dirname + '/css/index.css');
-});
+//app.get('/css/index.css', function (req, res) {
+    //res.sendFile(__dirname + '/css/index.css');
+//});
 
 app.listen(process.env.PORT || port, () => console.log(`${port}`));
