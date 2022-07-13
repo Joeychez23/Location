e2f480fe412658eaf2143c99b2c4ef12ca00fdd5
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-12 22:24:40 -0700

    wtf

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 //const Datastore = require('nedb');
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(process.env.PORT || port, () => console.log(`${port}`));
