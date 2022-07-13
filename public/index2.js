a4d1445ff0242a829a547f4382376bca403dac46
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-12 23:14:36 -0700

    try

--- public/index2.js
+++ public/index2.js
@@ -37,65 +37,120 @@ function setup() {
     if ('geolocation' in navigator) {
         console.log('geolocation available');
         navigator.geolocation.getCurrentPosition(async position => {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            document.getElementById('latitude').textContent = lat.toFixed(2);
-            document.getElementById('longitude').textContent = lon.toFixed(2);
-            //Weather API
+            try {
+                lat = position.coords.latitude;
+                lon = position.coords.longitude;
+                document.getElementById('latitude').textContent = lat.toFixed(2);
+                document.getElementById('longitude').textContent = lon.toFixed(2);
+                //Weather API
 
 
 
 
-            const api_key = "293a3bc9a37743dfb48df9cfd0d44dc1";
-            const api_url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${api_key}&include=minutely&units=I`;
-            const weather_response = await fetch(api_url);
-            const castData = await weather_response.json();
+                const api_key = "293a3bc9a37743dfb48df9cfd0d44dc1";
+                const api_url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${api_key}&include=minutely&units=I`;
+                const weather_response = await fetch(api_url);
+                const castData = await weather_response.json();
 
 
-            city_name = castData.data[0].city_name;
-            weather = castData.data[0].weather.description;
-            temp = castData.data[0].temp;
+                city_name = castData.data[0].city_name;
+                weather = castData.data[0].weather.description;
+                temp = castData.data[0].temp;
 
-            document.getElementById('cityName').textContent = city_name;
-            document.getElementById('weather').textContent = weather;
-            document.getElementById('temp').textContent = temp;
-
-
-
-
-            const aq_url = `https://api.openaq.org/v2/latest?coordinates=${lat},${lon}`;
-            const aq_response = await fetch(aq_url);
-            const aq_data = await aq_response.json();
+                document.getElementById('cityName').textContent = city_name;
+                document.getElementById('weather').textContent = weather;
+                document.getElementById('temp').textContent = temp;
 
 
 
 
-            air = aq_data.results[0].measurements[0];
-
-            document.getElementById('aq_parameter').textContent = air.parameter;
-            document.getElementById('aq_value').textContent = air.value;
-            document.getElementById('aq_units').textContent = air.unit;
-            document.getElementById('aq_date').textContent = air.lastUpdated;
+                const aq_url = `https://api.openaq.org/v2/latest?coordinates=${lat},${lon}`;
+                const aq_response = await fetch(aq_url);
+                const aq_data = await aq_response.json();
 
 
 
 
+                air = aq_data.results[0].measurements[0];
 
-            const data = {lat, lon, city_name, weather, temp , air}; //, name, image64};
-            const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json' 
-            },
-            body: JSON.stringify(data)
+                document.getElementById('aq_parameter').textContent = air.parameter;
+                document.getElementById('aq_value').textContent = air.value;
+                document.getElementById('aq_units').textContent = air.unit;
+                document.getElementById('aq_date').textContent = air.lastUpdated;
+
+
+
+
+
+                const data = {lat, lon, city_name, weather, temp , air}; //, name, image64};
+                const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json' 
+                },
+                body: JSON.stringify(data)
+        
+                };
+                console.log(options)
+                const response = await fetch('/api', options);
+                const db_val = await response.json();
+                console.log(db_val);
+            } catch(error) {
+                document.getElementById('cityName').textContent = city_name;
+                document.getElementById('weather').textContent = weather;
+                document.getElementById('temp').textContent = temp;
+                document.getElementById('air_con').textContent = 'No reading available in this area :(';
+
+                //document.getElementById("issMap").style.height = "260px";
+                //document.getElementById("issMap").style.visibility = "visible";
+
+
+                //name value
+                //const name = nameStr.value;
+                //Load Image into base64
+                //media.loadPixels();
+                //const image64 = media.canvas.toDataURL();
+                //data.image64 = image64;
+                const data = {lat, lon, city_name, weather, temp}; //, name, image64};
+                const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json' 
+                },
+                body: JSON.stringify(data)
     
-            };
-            console.log(options)
-            const response = await fetch('/api', options);
-            const db_val = await response.json();
-            console.log(db_val);
-
+                };
+                //console.log(options)
+                const response = await fetch('/api', options);
+                const db_val = await response.json();
+                console.log(db_val);
+            }
+            //console.log(position);
+            //const nameStr = document.getElementById('nameBox');
+            //const data = {lat, lon};
+            //document.getElementById('subData').addEventListener("click", async function() {
+                //name value
+                //const name = nameStr.value;
+                //Load Image into base64
+                //media.loadPixels();
+                //const image64 = media.canvas.toDataURL();
+                //data.image64 = image64;
+                //const data = {lat, lon, city_name}; //, name, image64};
+                //const options = {
+                //method: 'POST',
+                //headers: {
+                //    'Content-Type': 'application/json' 
+                //},
+                //body: JSON.stringify(data);
+                //};
+                //console.log(options)
+                //const response = await fetch('/api', options);
+                //const val = await response.json();
+                //console.log(val);
+            //});
         });
+    } else {
+        console.log('geolocation not available');
     }
 }
 
