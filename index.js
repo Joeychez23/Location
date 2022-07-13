f4676c46f79ce4da68c36587d8cf8d2a1e647bc7
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-12 22:48:48 -0700

    css

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const port = 3000;
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/css/index.css', function (req, res) {
+    res.sendFile(__dirname + '/css/index.css');
+});
 
 app.listen(process.env.PORT || port, () => console.log(`${port}`));
