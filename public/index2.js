db01a02195e0847c4dede2e7507235859434cbb9
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-14 15:18:25 -0700

    c

--- public/index2.js
+++ public/index2.js
@@ -63,25 +63,25 @@ function setup() {
 
 
 
-                const aq_url = `https://api.openaq.org/v2/latest?coordinates=${lat},${lon}`;
-                const aq_response = await fetch(aq_url);
-                const aq_data = await aq_response.json();
+                //const aq_url = `https://api.openaq.org/v2/latest?coordinates=${lat},${lon}`;
+                //const aq_response = await fetch(aq_url);
+                //const aq_data = await aq_response.json();
 
 
 
 
-                air = aq_data.results[0].measurements[0];
+                //air = aq_data.results[0].measurements[0];
 
-                document.getElementById('aq_parameter').textContent = air.parameter;
-                document.getElementById('aq_value').textContent = air.value;
-                document.getElementById('aq_units').textContent = air.unit;
-                document.getElementById('aq_date').textContent = air.lastUpdated;
-
-
+                //document.getElementById('aq_parameter').textContent = air.parameter;
+                //document.getElementById('aq_value').textContent = air.value;
+                //document.getElementById('aq_units').textContent = air.unit;
+                //document.getElementById('aq_date').textContent = air.lastUpdated;
 
 
 
-                const data = {lat, lon, city_name, weather, temp , air}; //, name, image64};
+
+
+                const data = {lat, lon, city_name, weather, temp}//air}; //, name, image64};
                 const options = {
                 method: 'POST',
                 headers: {
