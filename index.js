6310ebb2ea9c1ff4ef5ac83e5927bed0cd7bbe75
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-14 22:55:35 -0700

    L

--- index.js
+++ index.js
@@ -13,15 +13,47 @@ app.use(express.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
-
-const connection = mysql.createConnection({
+var config = mysql.createConnection({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'bc7ada4f2cece9',
     password: '084c2219',
     database: 'heroku_e723bcfa51ec52b',
     multipleStatements: true
 });
+
+
+var connection 
+
+function handleDisconnect() {
+    connection = mysql.createConnection(config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+}
+  
+handleDisconnect();
+
+/*= mysql.createConnection({
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bc7ada4f2cece9',
+    password: '084c2219',
+    database: 'heroku_e723bcfa51ec52b',
+    multipleStatements: true
+});*/
 
 
 connection.connect(function(err) {
