093dc4b187e934212bc0c653de33c67e1fc1e272
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-15 15:56:33 -0700

    l

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-require('dotenv').config();
+//require('dotenv').config();
 const mysql = require('mysql');
 //const db = require('./config/db');
 const port = process.env.PORT || 3000;
