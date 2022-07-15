9fd70aaa4a58a49b310edb06b241b51da2b65934
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-14 19:38:37 -0700

    add

--- public/index2.js
+++ public/index2.js
@@ -24,7 +24,7 @@ function setup() {
        //document.getElementById('video').srcObject = stream;
     //});
 
-    setInterval(window.location.reload(), 60000);
+    setInterval(window.location.reload(), 50000);
 
 
     //Create video to use base64 data
