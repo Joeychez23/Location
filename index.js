52062f25f0bfd36b778f4a7ebeba67ae2eee7c60
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-14 18:27:39 -0700

    ha

--- index.js
+++ index.js
@@ -155,6 +155,7 @@ console.log(`listening on port ${port}`);
 
 app.post('/api', (request, response) => {
     let data = request.body;
+    try{
     connection.connect(function(err){   
         if(err) throw connection.end();
         console.log("database connected");
@@ -181,6 +182,35 @@ app.post('/api', (request, response) => {
         })
         //connection.end();
     });
+} catch (err) {
+    connection.connect(function(err){   
+        if(err) throw connection.end();
+        console.log("database connected");
+        var sql = "SET @id = ?;SET @lat = ?;SET @lon = ?;SET @city_name = ?;SET @weather = ?;SET @temp = ?; \
+        CALL dataAddOrEdit(@id,@lat,@lon,@city_name,@weather,@temp);";
+        connection.query(sql, [data.id, data.lat, data.lon, data.city_name, data.weather, data.temp], (error, rows, fields) => {
+            if (error) {
+                //console.log(error);
+                connection.end();
+            }
+            if (!error) {
+                console.log(rows);
+                rows.forEach(element => {
+                if(element.constructor == Array);
+                })
+                console.log(request.body);
+                //const data = request.body;
+                const timestamp = Date.now();
+                data.timestamp = timestamp;
+                response.json(data);
+                console.log('Success');
+                connection.end;
+            }    
+        })
+        //connection.end();
+    });
+
+}
     //connection.end();
 });
 
